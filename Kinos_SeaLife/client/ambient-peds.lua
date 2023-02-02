3b8f518be2de0c8a3bdd47f532054e0024875447
SinKino <100452268+SinKino@users.noreply.github.com>
2023-02-01 23:35:04 -0500

    Add files via upload

---
+++ Kinos_SeaLife/client/ambient-peds.lua
@@ -0,0 +1,195 @@
+CreateThread(function()
+	OceanPeds()
+end)
+
+
+function OceanPeds()
+    
+    --Dolphins
+    --1
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1546.58, -1393.22, -1.04, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1546.58, -1393.22, 0.04, 14.0, 5.0, 5.0)
+    --2
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1561.74, -1402.48, -0.75, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1561.74, -1402.48, 0.75, 14.0, 5.0, 5.0)
+    --3
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1585.11, -1411.24, -2.51, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1585.11, -1411.24, -2.11, 14.0, 5.0, 5.0)
+    --4
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1594.06, -1421.7, -1.72, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1594.06, -1421.7, -1.22, 14.0, 5.0, 5.0)
+    --5
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1632.29, -1409.64, -1.07, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1632.29, -1409.64, -0.67, 14.0, 5.0, 5.0)
+    --6
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1742.72, -1364.42, -14.36, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1742.72, -1364.42, -14.36, 14.0, 5.0, 5.0)
+    --7
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1773.77, -1407.81, -19.37, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1773.77, -1407.81, -19.37, 14.0, 5.0, 5.0)
+    --8
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1842.18, -1368.63, -20.69, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1842.18, -1368.63, -20.69, 14.0, 5.0, 5.0)
+    --9
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1809.54, -1287.42, -8.9, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1809.54, -1287.42, -8.9, 14.0, 5.0, 5.0)
+    --10
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1716.43, -1300.12, -12.92, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1716.43, -1300.12, -12.92, 14.0, 5.0, 5.0)
+    --11
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1678.07, -1370.37, -6.83, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1678.07, -1370.37, -6.83, 14.0, 5.0, 5.0)
+    --12
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1719.64, -1431.3, -16.07, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1719.64, -1431.3, -16.07, 14.0, 5.0, 5.0)
+    --13
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1632.29, -1409.64, -1.07, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1632.29, -1409.64, -0.67, 14.0, 5.0, 5.0)
+    --14
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1728.51, -1294.63, -5.3, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1728.51, -1294.63, -5.3, 14.0, 5.0, 5.0)
+    --15
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1791.14, -1391.03, -23.68, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1791.14, -1391.03, -23.68, 14.0, 5.0, 5.0)
+    --16
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1869.45, -1319.67, -11.89, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1869.45, -1319.67, -11.89, 14.0, 5.0, 5.0)
+    --17
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1931.02, -1259.72, -10.22, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1931.02, -1259.72, -10.22, 14.0, 5.0, 5.0)
+    --18
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1914.07, -1190.26, -9.84, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1914.07, -1190.26, -9.84, 14.0, 5.0, 5.0)
+    --19
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1730.08, -1619.37, -6.08, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1730.08, -1619.37, -6.08, 14.0, 5.0, 5.0)
+    --20
+    model = GetHashKey("a_c_dolphin")
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    ped = CreatePed(0, model , -1619.01, -1354.3, -3.91, true)
+    SetEntityInvincible(ped, false)
+    TaskWanderInArea(ped, -1619.01, -1354.3, -3.91, 14.0, 5.0, 5.0)
+    
+    
+
+   
+
+end
+
